484e0afb231c12d99f60f5d9c944f23979c0374d
Summer Young <summer.joy.young@gmail.com>
2024-08-16 08:34:13 -0700

    Add an astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://glowing-hummingbird-ebaf90.netlify.app"
+  site: "https://glowing-hummingbird-ebaf90.netlify.app",
+  integrations: [preact()]
 });
